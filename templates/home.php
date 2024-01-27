90cb1b840637c60bb8d61adf5028c97b81aa9e8e
Olga <112238837+OlgaSmile@users.noreply.github.com>
2024-01-27 21:44:10 +0200

    add fonts, general css for container, primary&secondary buttons, hero section

--- templates/home.php
+++ templates/home.php
@@ -4,7 +4,36 @@ Template Name: home
 */
 get_header();
 ?>
-<h2>home page</h2>
+<main class="front-page__main">
+  <section class="hero-section">
+    <div class="container">
+      <div class="inner-container hero-inner__container">
+        <div class="hero-wrapper__title">
+          <h2 class="hero-section__subtitle"><?php echo get_field('hero_subtitle'); ?></h2>
+          <h1 class="hero-section__title"><?php echo get_field('hero_title'); ?></h1>
+        </div>
+
+        <div class="hero-wrapper__img">
+          <?php 
+            $image = get_field('hero_photo');
+            $size = 'large';
+            if( $image ) {
+                echo wp_get_attachment_image( $image, $size );
+          } ?>
+        </div>
+
+        <div class="hero-wrapper__text">
+          <p class="hero-section__text"><?php echo get_field('hero_text'); ?></p>
+          <div class="hero-btn__container">
+            <a class="button hero-button primary-button" href=""><?php echo get_field('support_btn', 'option'); ?></a>
+            <a class="button hero-button secondary-button"
+              href=""><?php echo get_field('event_calendar_btn', 'option'); ?></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
 
 
 
