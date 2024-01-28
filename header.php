66553e5c37b1c592aaec4d931091a6ed18be96aa
Diana Forostiana <dianaforost@gmail.com>
2024-01-28 23:56:45 +0100

    feat: header

--- header.php
+++ header.php
@@ -1,45 +1,66 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
-    <title>It-volunteers</title>
+    <title></title>
 </head>
+
 <body>
     <div class="wrapper">
-        <header class="header">        
-            <div class="header__content _container">
-                <div class="header__menu menu">
-                    <div class="menu__icon icon-menu menu__round">
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                    </div>
+    <header class="header">
+        <div class="header__container">
+               
+                <div class="menu__body" id="">
+                               <div class="menu__container">
+                                    <div class="menu__content">
+                                        <div class="menu__close-button"><svg class="menu__icon">
+                                <use href="<?php echo get_template_directory_uri()?>/assets/images/sprite.svg#close"></use>
+                            </svg></div>
+                                    <?php wp_nav_menu([
+                                            'theme_location'       => 'header',
+                                            'container'            => false,
+                                            'menu_class'           => 'menu__list',
+                                            'menu_id'              => false,
+                                            'echo'                 => true,
+                                            'items_wrap'           => '<ul id="%1$s" class="header__first__list %2$s">%3$s</ul>',
+                                        ]);
+                                        ?>
+                                        <div class="header__logo">
+                                            <?php
+                                            if (has_custom_logo()) {
+                                                echo get_custom_logo();
+                                            }
+                                            ?>
+                                        
+                                        </div>
+                            <ul class="header__second__list">
+                                <li class="menu-item">
+                                        <a href="<?php echo esc_attr(get_field('first-item-link', 'option') ); ?>"><?php the_field('first-item-name', 'option'); ?></a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a href="<?php echo esc_attr(get_field('last-item-link', 'option') ); ?>"><?php the_field('last-item-name', 'option'); ?></a>
+                                    </li>
+                                    <li>UKR</li>
+                                    <li><a class="secondary_button button header__button" href="<?php echo esc_attr(get_field('header-button-link', 'option') ); ?>" >
+                                        <?php the_field('header-button-name', 'option'); ?></a></li>
+                            </ul>
+                                    </div>
+                               </div>
+                               <div class="header__burger__logo">
+                                            <?php
+                                            if (has_custom_logo()) {
+                                                echo get_custom_logo();
+                                            }
+                                            ?>
+                                    
+                                        </div>
+                      <div class="burger">
+                          <span></span>
+                      </div>
                 </div>
-                <div class="menu__nav">  
-                    <?php 
-                        if ( has_custom_logo() ) {
-                            echo get_custom_logo();
-                        }
-                    ?>                     
-                    <div class="menu__content">IT VOLUNTEERS</div>
-                    <nav class="menu__body"> 
-                        <div class=" menu__container">
-                            <?php wp_nav_menu( [
-                                'theme_location'       => 'header',                          
-                                'container'            => false,                           
-                                'menu_class'           => 'menu__list',
-                                'menu_id'              => false,    
-                                'echo'                 => true,                            
-                                'items_wrap'           => '<ul id="%1$s" class="header_list %2$s">%3$s</ul>',  
-                                ] ); 
-                            ?>   
-                        </div>                          
-                    </nav> 
-                    <div class="burger-menu__overlay"></div> 
-                </div>                
-            </div>                      
-        </header>  
-	
+        </div>
+    </header>
--- src/scripts/header.js
+++ src/scripts/header.js
@@ -1,1 +1,21 @@
-console.log("header");
+const menu = document.querySelector(".menu__container");
+const openButton = document.querySelector(".burger");
+const closeButton = document.querySelector(".menu__close-button");
+
+openButton.addEventListener("click", function () {
+  menu.classList.toggle("open");
+  document.body.classList.toggle("modal-open");
+  menu.addEventListener("click", closeByBgdClick);
+  closeButton.addEventListener("click", hideMenu);
+});
+function hideMenu() {
+  menu.classList.remove("open");
+  document.body.classList.remove("modal-open");
+  closeButton.removeEventListener("click", hideMenu);
+  menu.removeEventListener("click", closeByBgdClick);
+}
+function closeByBgdClick(e) {
+  if (e.target === menu) {
+    hideMenu();
+  }
+}
