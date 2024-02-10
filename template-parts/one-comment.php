c085295554d4932b109b69edbcf890fb66287c25
Olga <112238837+OlgaSmile@users.noreply.github.com>
2024-02-10 17:58:44 +0200

    add mobile adaptation in the feedback article

--- template-parts/one-comment.php
+++ template-parts/one-comment.php
@@ -2,17 +2,19 @@
   <?php 
     $fullContent = $post->post_content;
 
-    $content = wp_trim_words($post->post_content);
-    $excerpt = wp_trim_words($post->post_content, 40); 
+    $content = wp_trim_words($post->post_content, 80);
+    $contentMobile = wp_trim_words($post->post_content, 60);
+    $contentDesktop = wp_trim_words($post->post_content, 40);  
 
-    $excerptLength = strlen($excerpt);
-    $contentLength = strlen($content); 
-    
-    $randomNumber = rand(200, 280);?>
+    $excerptMobileLength = strlen($contentMobile);
+    $excerptDesktopLength = strlen($contentDesktop);
+    $fullContentLength = strlen($content); 
 
-  <div class="feedback-post__box">
-    <?php if ($excerptLength < $contentLength) { ?>
-    <div id="read-more-container-js" class="feedback-post__excerpt" style="height: <?php echo $randomNumber?>px;">
+    $randomNumberDesktop = rand(100, 240);?>
+
+  <div class="feedback-post__box feedback-post__box-mobile">
+    <?php if ($excerptMobileLength < $fullContentLength) { ?>
+    <div id="read-more-container-js" class="feedback-post__excerpt" style="height: 320px;">
       <?php if($fullContent) echo $fullContent; ?>
     </div>
     <button id="read-more-js" class='feedback-open__btn' type='button' onclick='
@@ -25,11 +27,39 @@
         fullText.removeAttribute("style");
       } else {
         this.innerHTML = `<?php the_field("read_more_btn", "option"); ?>`;
-        fullText.style.height = `<?php echo $randomNumber?>px`;
+        fullText.style.height = `320px`;
         fullText.classList.remove("open");
       }
       '>
+      <?php the_field('read_more_btn', 'option'); ?></button>
 
+    <?php } else { ?>
+    <div class="feedback-post__content">
+      <?php if($fullContent) echo $fullContent; ?>
+    </div>
+    <?php } ?>
+  </div>
+
+  <div class="feedback-post__box feedback-post__box-desktop">
+    <?php if ($excerptDesktopLength < $fullContentLength) { ?>
+    <div id="read-more-container-js" class="feedback-post__excerpt"
+      style="height: <?php echo $randomNumberDesktop?>px;">
+      <?php if($fullContent) echo $fullContent; ?>
+    </div>
+    <button id="read-more-js" class='feedback-open__btn' type='button' onclick='
+
+      const fullText = this.previousElementSibling;
+
+      if(fullText.hasAttribute("style")){
+        this.innerHTML = `<?php the_field("hide_btn", "option"); ?>`;
+        fullText.classList.add("open");
+        fullText.removeAttribute("style");
+      } else {
+        this.innerHTML = `<?php the_field("read_more_btn", "option"); ?>`;
+        fullText.style.height = `<?php echo $randomNumberDesktop?>px`;
+        fullText.classList.remove("open");
+      }
+      '>
       <?php the_field('read_more_btn', 'option'); ?></button>
 
     <?php } else { ?>
