ac159ca3ddd6b712f7ad620d53ef279475399585
Olga Barnikova <barnikovaolga8@gmail.com>
2024-02-27 16:17:51 +0200

    fixed contact-form, added imask.js

--- functions.php
+++ functions.php
@@ -29,7 +29,8 @@ function wp_it_volunteers_scripts() {
   wp_enqueue_script( 'jquery-scripts', 'https://code.jquery.com/ui/1.12.1/jquery-ui.min.js', array(), false, true );
   wp_enqueue_script('swiper-scripts', 'https://cdn.jsdelivr.net/npm/swiper@10.0.0/swiper-bundle.min.js', array(), false, true);  
   wp_enqueue_script('choices-scripts', 'https://cdn.jsdelivr.net/npm/choices.js/public/assets/scripts/choices.min.js', array(), false, true);
-   
+  wp_enqueue_script('imask-scripts', 'https://unpkg.com/imask', array(), false, true);
+
     if ( is_page_template('templates/home.php') ) {
         wp_enqueue_style( 'home-style', get_template_directory_uri() . '/assets/styles/template-styles/home.css', array('main') );
         wp_enqueue_script( 'home-scripts', get_template_directory_uri() . '/assets/scripts/template-scripts/home.js', array(), false, true );
