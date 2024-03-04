ed0901ad37aaf73336b4690570120acb4164d648
Diana Forostiana <dianaforost@gmail.com>
2024-03-04 11:06:56 +0100

    fix: styles

--- template-parts/projects-list.php
+++ template-parts/projects-list.php
@@ -34,13 +34,50 @@
                          echo '<a class="button secondary-button projects__section__swiper__item__button" href='. $project_link .'>'. $projects_button .'</a>';
                          echo '</div>';
                          if (!empty($project_images)) {
-                          echo '<div class="photo">';
+                          echo '<div class="projects__section__swiper__item__photo__container">';
                           if (count($project_images) === 1) {
                               $image = $project_images[0];
-                              echo '<img class="photo__photo single-image" loading="lazy" src="' . esc_url($image['url']) . '" alt="' . esc_attr($image['alt']) . '">';
+                              echo '<img class="projects__section__swiper__item__photo single-image" loading="lazy" src="' . esc_url($image['url']) . '" alt="' . esc_attr($image['alt']) . '">';
                           } else {
                               foreach ($project_images as $index => $image) {
-                                  echo '<img class="photo__photo" loading="lazy" src="' . esc_url($image['url']) . '" alt="' . esc_attr($image['alt']) . '">';
+                                  echo '<img class="projects__section__swiper__item__photo" loading="lazy" src="' . esc_url($image['url']) . '" alt="' . esc_attr($image['alt']) . '">';
+                              }
+                          }
+                          echo '</div>';
+                      }
+                        echo '</div>';  
+
+
+
+
+
+
+
+
+
+
+
+                        echo '<div class="swiper-slide projects__section__swiper__item">';
+                        if ($terms && !is_wp_error($terms)) {
+                            foreach ($terms as $term) {
+                                echo '<div class="projects__section__swiper__item__mark"><p class="projects__section__swiper__item__mark__message">' . $term->name . '</p></div>';
+                            }
+                        }
+                         echo '<div class="projects__section__swiper__item__content">';
+                         echo '<div class="projects__section__swiper__item__content__wrapper">';
+                         echo '<h2 class="projects__section__swiper__item__title">' . $project_title . '</h2>';
+                         echo '<p class="projects__section__swiper__item__description">' . $project_description . '</p>';
+                         echo '</div>';
+                         echo '<a class="button secondary-button projects__section__swiper__item__button" href='. $project_link .'>'. $projects_button .'</a>';
+                         echo '</div>';
+                         if (!empty($project_images)) {
+                          echo '<div class="projects__section__swiper__item__photo__container">';
+                          if (count($project_images) === 1) {
+                              $image = $project_images[0];
+                              echo '<img class="projects__section__swiper__item__photo single-image" loading="lazy" src="' . esc_url($image['url']) . '" alt="' . esc_attr($image['alt']) . '">';
+                          } else {
+                              foreach ($project_images as $index => $image) {
+                                  echo '<img class="projects__section__swiper__item__photo" loading="lazy" src="' . esc_url($image['url']) . '" alt="' . esc_attr($image['alt']) . '">';
                               }
                           }
                           echo '</div>';
