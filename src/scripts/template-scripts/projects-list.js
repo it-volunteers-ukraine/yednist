8bcfd3c38593a6e5401c5897b846dd22ecbbb527
Diana Forostiana <dianaforost@gmail.com>
2024-03-07 19:45:32 +0100

    fix: js

--- src/scripts/template-scripts/projects-list.js
+++ src/scripts/template-scripts/projects-list.js
@@ -54,4 +54,29 @@ document.addEventListener("DOMContentLoaded", function () {
       },
     },
   });
+
+  // Scroll
+  const paginationItems = document.querySelectorAll(
+    ".projects__section .swiper-pagination-bullet"
+  );
+  const prevButton = document.querySelector(
+    ".projects__section .custom-button-prev"
+  );
+  const nextButton = document.querySelector(
+    ".projects__section .custom-button-next"
+  );
+  if (paginationItems.length !== 0) {
+    paginationItems.forEach((item) => {
+      item.addEventListener("click", scrollToTop);
+    });
+    prevButton.addEventListener("click", scrollToTop);
+    nextButton.addEventListener("click", scrollToTop);
+  }
+
+  function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }
 });
