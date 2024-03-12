cce7fa63831f784fc465ea3a9f2782e144e361cf
Olga Barnikova <barnikovaolga8@gmail.com>
2024-03-12 17:02:16 +0200

    resolving conflict

--- functions.php
+++ functions.php
@@ -51,10 +51,6 @@ function wp_it_volunteers_scripts() {
         wp_enqueue_style( 'partners-style', get_template_directory_uri() . '/assets/styles/template-styles/partners.css',array('main'));
     }
 
-    if ( is_page_template('templates/projects.php')) {
-      wp_enqueue_style( 'projects-style', get_template_directory_uri() . '/assets/styles/template-styles/projects.css',array('main'));
-      wp_enqueue_script( 'projects-scripts', get_template_directory_uri() . '/assets/scripts/template-scripts/projects.js', array(), false, true );
-  }
     if ( is_page_template('templates/gallery-photo.php') ) {
       wp_enqueue_style( 'gallery-photo-style', get_template_directory_uri() . '/assets/styles/template-styles/gallery-post.css', array('main') );
       wp_enqueue_script( 'fslightbox-scripts', get_template_directory_uri() . '/assets/scripts/template-scripts/fslightbox.js', array(), false, true );
