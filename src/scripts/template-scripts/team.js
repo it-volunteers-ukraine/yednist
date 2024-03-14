4d876f6004637e370bd9ac76195c3c5ba9fcd0eb
Olga <112238837+OlgaSmile@users.noreply.github.com>
2024-03-14 17:53:44 +0200

    Revert "Merge pull request #68 from it-volunteers-ukraine/team"
    This reverts commit cee63fd75be4896985f0d3ea44e17fa5f9eb139e, reversing
    changes made to 5897d618d85816a8dc702d0c6c76f56a8ce2d8e4.

--- src/scripts/template-scripts/team.js
+++ src/scripts/template-scripts/team.js
@@ -1,93 +1,41 @@
-
-
-const buttons = document.querySelectorAll('.show-info');
-
-let swiper = Swiper;
-let init = false;
-
-function swiperMode() {
-
-
-    const mobile = window.matchMedia('(min-width: 0) and (max-width: 575px)');
-    const tablet = window.matchMedia('(min-width: 576px) and (max-width: 991px)');
-    const desktop = window.matchMedia('(min-width: 1025px)');
-
-
-    if (mobile.matches || tablet.matches) {
-
-        if (!init) {
-
-            init = true;
-
-            swiper = new Swiper('.team_slider', {
-                loop: true,
-                autoplay: {
-                    delay: 2000,
-                },
-                pagination: {
-                    el: '.swiper-pagination',
-                },
-                slidesPerView: 1,
-                observer: true,
-                observeParents: true,
-                autoWidth: false,
-                breakpoints: {
-                    576: {
-                        slidesPerView: 2,
-                        spaceBetween: 28,
-                    },
-                },
-            });
-
+const teamSwiper = new Swiper(".swiper", {
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    spaceBetween: 30,
+    loop: true,
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            enabled: true
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 28,
+            enabled: true,
+        },
+        1199: {
+            slidesPerView: 2,
+            spaceBetween: 28,
+            enabled: true,
+            grid: {
+                rows: 3,
+            },
+        },
+        1200: {
+            enabled: false,
+            grid: {
+                rows: 3,
+            },
+            slidesPerView: 3,
         }
-    } else if (desktop.matches) {
-
-        swiper.destroy();
-        init = false;
-    }
-}
-
-
-window.addEventListener('load', function () {
-    swiperMode();
-});
-
-
-window.addEventListener('resize', function () {
-    swiperMode();
-});
-
-
-
-
-function showInfo(event) {
-
-    const targetButton = event.currentTarget;
-
-    const infoByButton = targetButton.nextElementSibling;
-
-
-    targetButton.classList.toggle('active');
-
-
-    infoByButton.style.display = (targetButton.classList.contains('active')) ? 'block' : 'none';
-
-
-    if (!targetButton.classList.contains('active')) {
-        targetButton.classList.add('inactive');
-    } else {
-        targetButton.classList.remove('inactive');
-    }
-
-}
-
-
-buttons.forEach(function (button) {
-    button.addEventListener("click", function (event) {
-        showInfo(event);
-    })
-
+    },
 })
+
+
+
+
 
 
 
