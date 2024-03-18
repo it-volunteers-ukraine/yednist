3c0b747cf3ec7d895668fe04281dffc660506eed
Diana Forostiana <dianaforost@gmail.com>
2024-03-18 15:04:33 +0100

    feat: stages section

--- templates/boarding-school.php
+++ templates/boarding-school.php
@@ -10,29 +10,33 @@ get_header();
 <section class="section">
   <div class="container">
     <div class="inner-container">
-    <h2 class="page-title main__section__title"><?php the_title(); ?></h2>
-    <div class="main__section__content">
-    <?php $main_photo = get_field("main_photo"); $quotation_text = get_field("quotation_text"); $video_link = get_field('video_link'); ?>
-      <div class="main__section__content__image">
-      <?php if ($main_photo): ?>
-        <img src="<?php echo esc_url($main_photo['url']); ?>" alt="<?php echo esc_attr($main_photo['alt']); ?>" style="display: block; margin: 0 auto;">
-        <svg class="main__section__content__image__icon">
-          <use href="<?php echo get_template_directory_uri() ?>/assets/images/sprite.svg#icon-youtube"></use>
-        </svg>
-      <?php endif; ?>
-      <div id="videoModal" class="modal">
-      <div class="modal-content">
-        <iframe src='<?php echo $video_link ?>' frameborder="0" allowfullscreen autoplay></iframe>
+      <h2 class="page-title main__section__title"><?php the_title(); ?></h2>
+      <div class="main__section__content">
+        <?php 
+          $main_photo = get_field("main_photo"); 
+          $quotation_text = get_field("quotation_text"); 
+          $video_link = get_field('video_link'); 
+        ?>
+        <div class="main__section__content__image">
+          <?php if ($main_photo): ?>
+            <img src="<?php echo esc_url($main_photo['url']); ?>" alt="<?php echo esc_attr($main_photo['alt']); ?>" style="display: block; margin: 0 auto;">
+            <svg class="main__section__content__image__icon">
+              <use href="<?php echo get_template_directory_uri() ?>/assets/images/sprite.svg#icon-youtube"></use>
+            </svg>
+          <?php endif; ?>
+          <div id="videoModal" class="modal">
+            <div class="modal-content">
+              <iframe src='<?php echo $video_link ?>' frameborder="0" allowfullscreen autoplay></iframe>
+            </div>
+          </div>
+        </div>
+        <div class="main__section__content__quotation">
+          <svg class="main__section__content__quotation__icon">
+            <use href="<?php echo get_template_directory_uri() ?>/assets/images/sprite.svg#icon-bi_quote"></use>
+          </svg>
+          <p class="main__section__content__quotation__text"><?php echo $quotation_text ?></p>
+        </div>
       </div>
-    </div>
-      </div>
-      <div class="main__section__content__quotation">
-      <svg class="main__section__content__quotation__icon">
-        <use href="<?php echo get_template_directory_uri() ?>/assets/images/sprite.svg#icon-bi_quote"></use>
-      </svg>
-      <p class="main__section__content__quotation__text"><?php echo $quotation_text ?></p>
-      </div>
-    </div>
     </div>
   </div>
 </section>
@@ -41,12 +45,16 @@ get_header();
   <div class="container">
     <h3 class="section-title"><?php the_field("appeal_title") ?></h3>
     <div class="swiper inner-container appeal__swiper">
-    <div class="swiper-wrapper appeal__section__content"><?php while(have_rows('appeal_content')) : the_row(); 
-      $text = get_sub_field('appeal_text');
-    ?>
-      <div class="swiper-slide appeal__section__content__wrapper"><p class="appeal__section__content__wrapper__text"><?php echo $text ?></p></div>
-    <?php endwhile; ?></div>
-    <div class="swiper-pagination"></div>
+      <div class="swiper-wrapper appeal__section__content">
+        <?php while(have_rows('appeal_content')) : the_row(); 
+          $text = get_sub_field('appeal_text');
+        ?>
+        <div class="swiper-slide appeal__section__content__wrapper">
+          <p class="appeal__section__content__wrapper__text"><?php echo $text ?></p>
+        </div>
+        <?php endwhile; ?>
+      </div>
+      <div class="swiper-pagination"></div>
     </div>
   </div>
 </section>
@@ -86,6 +94,23 @@ get_header();
             <li class="school__section__list__item"><?php echo $text ?><li>
           <?php endwhile; ?>
       </ul>
+    </div>
+  </div>
+</section>
+
+<section class="section stages__section">
+  <div class="container">
+    <h3 class="section-title"><?php the_field("stages_title") ?></h3>
+    <div class="inner-container">
+      <div class="stages__section__wrapper">
+        <?php while(have_rows('stages_descriptions')) : the_row(); 
+          $text = get_sub_field('stages_description');
+        ?>
+        <p class="stages__section__wrapper__text"><?php echo $text ?></p>
+        <?php endwhile; ?>
+        <?php $image = get_field("stages_image"); ?>
+      </div>
+      <div class="stages__section__image-wrapper"><img class="stages__section__wrapper__image" src='<?php echo $image['url'] ?>' alt='<?php echo $image['alt'] ?>'/></div>
     </div>
   </div>
 </section>
