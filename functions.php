212af9c6f7a2c659f8d8727feeed475ebd4f2d82
Olga Barnikova <barnikovaolga8@gmail.com>
2024-03-19 12:15:49 +0200

    fixed breadcrumbs

--- functions.php
+++ functions.php
@@ -37,10 +37,10 @@ function wp_it_volunteers_scripts() {
         wp_enqueue_script( 'home-scripts', get_template_directory_uri() . '/assets/scripts/template-scripts/home.js', array('touch-swipe-scripts'), false, true );
     }
 
-    if ( is_page_template('templates/about.php') ) {
-        wp_enqueue_style( 'about-style', get_template_directory_uri() . '/assets/styles/template-styles/about.css', array('main') );
-        wp_enqueue_script( 'about-scripts', get_template_directory_uri() . '/assets/scripts/template-scripts/about.js', array(), false, true );
-    }
+    // if ( is_page_template('templates/about.php') ) {
+    //     wp_enqueue_style( 'about-style', get_template_directory_uri() . '/assets/styles/template-styles/about.css', array('main') );
+    //     wp_enqueue_script( 'about-scripts', get_template_directory_uri() . '/assets/scripts/template-scripts/about.js', array(), false, true );
+    // }
 
     if ( is_page_template('templates/contacts.php') ) {
         wp_enqueue_style( 'contacts-style', get_template_directory_uri() . '/assets/styles/template-styles/contacts.css', array('main') );
