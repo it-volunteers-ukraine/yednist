a392fbb42ad9ab94f89194c320c9f4bd45325004
Olga <112238837+OlgaSmile@users.noreply.github.com>
2024-03-29 09:35:28 +0200

    add dinamyc link to payU button

--- template-parts/support-details-btns.php
+++ template-parts/support-details-btns.php
@@ -1,6 +1,7 @@
 <div class="moneysupport__buttons--box">
-    <a href="/" class="button moneysupport__button"><svg>
-        <use href="<?php echo get_template_directory_uri()?>/assets/images/sprite.svg#icon_PayU">
-        </use>
-      </svg></a>
+  <a href="<?php echo esc_attr(get_field('support_button_url', 'options')); ?>"
+    class="button moneysupport__button"><svg>
+      <use href="<?php echo get_template_directory_uri()?>/assets/images/sprite.svg#icon_PayU">
+      </use>
+    </svg></a>
 </div>
