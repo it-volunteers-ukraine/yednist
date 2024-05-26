fed748ae08919de409f14025388e417299185a47
Olga <112238837+OlgaSmile@users.noreply.github.com>
2024-05-26 20:25:30 +0300

    change endpoints to acf

--- template-parts/support-details-btns.php
+++ template-parts/support-details-btns.php
@@ -2,7 +2,7 @@
   class="moneysupport__buttons--box">
   <div class="moneysupport__name-value">
     <label for="donation_amount" class="moneysupport__name"><?php the_field("payment_amount", "options") ?></label>
-    <input class="current_account" type="number" id="donation_amount" name="donation_amount" min="1" step="1"
+    <input class="current_account" type="number" id="donation_amount" name="donation_amount" min="1"
       placeholder="<?php the_field("enter_amount", "options") ?>" required>
   </div>
   <button type="submit" class="button moneysupport__button" aria-label="support by PayU">
