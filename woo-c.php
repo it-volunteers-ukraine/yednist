fd933a7d00fc8e5168a99dcb4d42f6376cb5498f
Diana Forostiana <dianaforost@gmail.com>
2024-07-03 19:15:48 +0200

    fix: container for woocommerce single product

--- woo-c.php
+++ woo-c.php
@@ -30,7 +30,16 @@ add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
 remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
 remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
 
+add_action('woocommerce_before_single_product', 'my_theme_product_wrapper_start', 5);
+add_action('woocommerce_after_single_product', 'my_theme_product_wrapper_end', 5);
 
+function my_theme_product_wrapper_start() {
+    echo '<div class="container"><div class="inner-container">';
+}
+
+function my_theme_product_wrapper_end() {
+    echo '</div></div>';
+}
 add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
 add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10);
 function my_theme_wrapper_start() {
