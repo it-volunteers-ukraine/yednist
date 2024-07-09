ca0a47cb0ffdcf480b9b36f2b32dc9f3be34d357
Olga <112238837+OlgaSmile@users.noreply.github.com>
2024-07-09 09:39:45 +0300

    Update breadcrumbs.php

--- template-parts/breadcrumbs.php
+++ template-parts/breadcrumbs.php
@@ -3,7 +3,7 @@
     <div class="breadcrumbs__line"></div>
     <div class="inner-container">
 
-      <div class="breadcrumbs" typeof="BreadcrumbList" vocab="https://schema.org/">
+      <div class="breadcrumbs" typeof="BreadcrumbList" vocab="https://schema.org/" style="line-height: 1;">
         <?php if(function_exists('bcn_display'))
 	        {
 		        bcn_display();
