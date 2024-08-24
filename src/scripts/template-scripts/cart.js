7c14a09c35fea38b6c7a10a3f23b9204281797dd
Olga <112238837+OlgaSmile@users.noreply.github.com>
2024-08-24 15:18:51 +0300

    add blur event, change padding in the hero

--- src/scripts/template-scripts/cart.js
+++ src/scripts/template-scripts/cart.js
@@ -1,4 +1,48 @@
 document.addEventListener("DOMContentLoaded", function () {
+  function updateCartQuantity(qtyInput) {
+    const productId = qtyInput
+      .closest("tr")
+      .querySelector("[data-product_id]")
+      .getAttribute("data-product_id");
+
+    const formData = new FormData();
+    formData.append("action", "update_cart_item_quantity");
+    formData.append("product_id", productId);
+    formData.append("quantity", qtyInput.value);
+
+    document.body.classList.add("woocommerce-processing");
+    document.querySelector("tbody").classList.add("processing");
+    document.querySelector("tbody").style.opacity = "0.5";
+    const blockOverlay = document.createElement("div");
+    blockOverlay.classList.add("blockUI", "blockOverlay");
+    document.querySelector("tbody").appendChild(blockOverlay);
+
+    fetch(wc_cart_params.ajax_url, {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        document.querySelector(".woocommerce-cart-form").innerHTML =
+          data.fragments[".woocommerce-cart-form"];
+        document.querySelector(".cart-collaterals").innerHTML =
+          data.fragments[".cart-totals"];
+        document
+          .querySelector("body")
+          .dispatchEvent(new Event("wc_cart_updated"));
+      })
+      .finally(() => {
+        document.body.classList.remove("woocommerce-processing");
+        document.querySelector("tbody").style.opacity = "1";
+        document.querySelector("tbody").classList.remove("processing");
+        var overlay = document.querySelector(".blockUI.blockOverlay");
+        if (overlay) {
+          overlay.remove();
+        }
+      })
+      .catch((error) => console.error("Ошибка:", error));
+  }
+
   document.addEventListener("click", function (event) {
     if (
       event.target.classList.contains("plus") ||
@@ -15,13 +59,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
       if (!currentVal || isNaN(currentVal)) currentVal = 0;
 
-      document.body.classList.add("woocommerce-processing");
-      document.querySelector("tbody").classList.add("processing");
-      document.querySelector("tbody").style.opacity = "0.5";
-      const blockOverlay = document.createElement("div");
-      blockOverlay.classList.add("blockUI", "blockOverlay");
-      document.querySelector("tbody").appendChild(blockOverlay);
-
       if (button.classList.contains("plus")) {
         if (max && currentVal >= max) {
           qtyInput.value = max;
@@ -37,41 +74,25 @@ document.addEventListener("DOMContentLoaded", function () {
       }
 
       qtyInput.dispatchEvent(new Event("change"));
-
-      const productId = button
-        .closest("tr")
-        .querySelector("[data-product_id]")
-        .getAttribute("data-product_id");
-
-      const formData = new FormData();
-      formData.append("action", "update_cart_item_quantity");
-      formData.append("product_id", productId);
-      formData.append("quantity", qtyInput.value);
-
-      fetch(wc_cart_params.ajax_url, {
-        method: "POST",
-        body: formData,
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          document.querySelector(".woocommerce-cart-form").innerHTML =
-            data.fragments[".woocommerce-cart-form"];
-          document.querySelector(".cart-collaterals").innerHTML =
-            data.fragments[".cart-totals"];
-          document
-            .querySelector("body")
-            .dispatchEvent(new Event("wc_cart_updated"));
-        })
-        .finally(() => {
-          document.body.classList.remove("woocommerce-processing");
-          document.querySelector("tbody").style.opacity = "1";
-          document.querySelector("tbody").classList.remove("processing");
-          var overlay = document.querySelector(".blockUI.blockOverlay");
-          if (overlay) {
-            overlay.remove();
-          }
-        })
-        .catch((error) => console.error("Ошибка:", error));
+      updateCartQuantity(qtyInput);
     }
   });
+
+  document.addEventListener("focusin", function (event) {
+    if (event.target.classList.contains("qty")) {
+      initialQtyValue = event.target.value;
+    }
+  });
+
+  document.addEventListener(
+    "blur",
+    function (event) {
+      if (event.target.classList.contains("qty")) {
+        if (event.target.value !== initialQtyValue) {
+          updateCartQuantity(event.target);
+        }
+      }
+    },
+    true
+  );
 });
